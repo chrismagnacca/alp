33971c56a95ce7a56bb15f5c042355dba8027226
Chris Magnacca <chrismagnacca@gmail.com>
2013-05-14 21:00:07 -0400

    modified staff and faculty page, updated css hover overs

--- Alp/Scripts/staffandfaculty.js
+++ Alp/Scripts/staffandfaculty.js
@@ -1,9 +1,12 @@
 ﻿$(function () {
     $('.bio-wrapper').mouseover(function () {
-        $(this).css('opacity', '1');
+        $(this).css('opacity', '1');        
+        $('#focused-image img').attr('src', $(this).children().first().attr('src'));
     });
 
     $('.bio-wrapper').mouseout(function () {
         $(this).css('opacity', '0.4');
     });
+
+
 });
