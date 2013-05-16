f50a13c04bd4f722b229cca965970997b28721f8
Chris Magnacca <chrismagnacca@gmail.com>
2013-05-15 20:21:52 -0400

    added in bio text, reorganized bio row based on experience, finished most of staff page

--- Alp/Scripts/staffandfaculty.js
+++ Alp/Scripts/staffandfaculty.js
@@ -2,11 +2,13 @@
     $('.bio-wrapper').mouseover(function () {
         $(this).css('opacity', '1');        
         $('#focused-image img').attr('src', $(this).children().first().attr('src'));
+        $('#bio-content div').html($(this).children().last().html());
     });
 
     $('.bio-wrapper').mouseout(function () {
         $(this).css('opacity', '0.4');
     });
 
-
+    $('#focused-image img').attr('src', $('#1-1 div.bio-wrapper').children().first().attr('src'));
+    $('#bio-content div').html($('#1-1 div.bio-text').html());
 });
