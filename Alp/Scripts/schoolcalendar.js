aad1bc346025a490f98506acce4f7f76db6d77b1
Chris Magnacca <chrismagnacca@gmail.com>
2013-06-01 19:49:37 -0400

    updated content spellings, staff page

--- Alp/Scripts/schoolcalendar.js
+++ Alp/Scripts/schoolcalendar.js
@@ -14,38 +14,7 @@
         },
         disableDragging: true,
         events: [
-            {
-                title: "Get Acquianted Day Sessions 1 & 2",
-                start: new Date("September 3, 2013 9:00:00"),
-                className: 'cal-event' 
-            },
-            {
-                title: "First Full Day of Preschool & Extended Care",
-                start: new Date("September 4, 2013 9:00:00"),
-                className: 'cal-event'
-            },
-            {
-                title: "All School Fall Fundraiser Begins",
-                start: new Date("September 11, 2013 9:00:00"),
-                end: new Date("September 11, 2013 5:00:00"),
-                className: 'cal-event'
-            },
-            {
-                title: "Parent Assembly, Church Sanctuary",
-                start: new Date("September 11, 2013 18:30:00"),
-                end: new Date("September 11, 2013 17:30:00"),
-                className: 'cal-event'
-            },
-            {
-                title: "All School Fall Fundraiser Ends",
-                start: new Date("September 25, 2013 9:00:00"),
-                className: 'cal-event'
-            },
-            {
-                title: "Fall School Pictures",
-                start: new Date("October 1, 2013 9:00:00"),
-                className: 'cal-event'
-            }
+
         ]
     });
 
